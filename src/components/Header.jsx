5c5719fec15a8e8351c0facdc2b42cdafedadfbe
qdw0719 <qdw0719@naver.com>
2021-09-04 20:16:33 +0900

    header - git address update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,8 +4,9 @@ export default function Header() {
             <div className="header__container">
                 <div className="title">Awesome Prototypes in Shop</div>
                 <div className="subtitle">
-                    First mini project after studying React
-                    by Park youngjun
+                    First mini project after studying React <br />
+                    by Park youngjun<br />
+                    git-address : <a href="https://github.com/qdw0719" style={{fontSize: "20px", color: "green", textDecoration: "none"}}>https://github.com/qdw0719</a>
                 </div>
                 <div className="btn__area">
                     <a href="https://www.protopie.io" target="_BLANK" rel="noreferrer">
